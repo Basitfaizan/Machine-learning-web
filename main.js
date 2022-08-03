51272542782b1cf331d989fc5cf7401652e4bff9
Basit Faizan <69634123+Basitfaizan@users.noreply.github.com>
2022-08-03 21:54:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('Hello World!');
+
+// start image slider
+let left = document.querySelector(".left")
+let right = document.querySelector(".right")
+let imageSliderContent = document.querySelector(".imageSliderContent")
+console.log('hello');
+
+left.addEventListener('click',()=>{
+    console.log('left');
+    let bigWidth = imageSliderContent.getBoundingClientRect();
+    imageSliderContent.scrollLeft -= bigWidth.width- 100;
+})
+right.addEventListener('click',()=>{
+    let bigWidth4 = imageSliderContent.getBoundingClientRect();
+    imageSliderContent.scrollLeft += bigWidth3.width+ 100;;
+})
+// end image slider
+// start navbar
+
+let hamburger = document.querySelector('.hamburger')
+let removeSidenav = document.querySelector('.removeSideNav')
+let sideNav = document.querySelector('.sideNav')
+hamburger.addEventListener('click',()=>{
+    sideNav.classList.add('active')
+})
+removeSidenav.addEventListener('click',()=>{
+    sideNav.classList.remove('active')
+})
